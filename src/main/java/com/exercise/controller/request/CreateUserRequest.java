2bc20f5280818158d508546c4f003664a54c2c7e
Daniele Vallesi <daniele.vallesi@mail.com>
2020-10-18 03:35:45 +0200

    Added Validation

--- src/main/java/com/exercise/controller/request/CreateUserRequest.java
+++ src/main/java/com/exercise/controller/request/CreateUserRequest.java
@@ -5,9 +5,13 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.validation.constraints.Min;
+
 @Getter @Setter @AllArgsConstructor @NoArgsConstructor
 public class CreateUserRequest {
     private String userUuid;
     private String name;
+
+    @Min(0) // This will get checked with @Value in the controller
     private int balance;
 }
