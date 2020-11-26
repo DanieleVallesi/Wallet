34dba4c1400bf17aadbfaa8be1689e23ba6f0064
Daniele Vallesi <daniele.vallesi@mail.com>
2020-11-26 04:13:21 +0100

    Modified Respose Entities types

--- src/main/java/com/exercise/controller/WalletController.java
+++ src/main/java/com/exercise/controller/WalletController.java
@@ -2,6 +2,8 @@ package com.exercise.controller;
 
 import com.exercise.controller.request.ChangeBalanceRequest;
 import com.exercise.controller.request.CreateUserRequest;
+import com.exercise.model.Transaction;
+import com.exercise.model.User;
 import com.exercise.service.WalletService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -9,6 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "/wallet")
@@ -18,22 +21,22 @@ public class WalletController {
     WalletService walletService;
 
     @PostMapping(path = "/change-user-balance")
-    public @ResponseBody ResponseEntity<Object> changeUserBalance(@RequestBody ChangeBalanceRequest body){
+    public @ResponseBody ResponseEntity<Integer> changeUserBalance(@RequestBody ChangeBalanceRequest body){
         return new ResponseEntity<>(walletService.changeUserBalance(body.getUserUuid(), body.getTransactionUuid(), body.getAmmount()), HttpStatus.OK) ;
     }
 
     @GetMapping(path = "/transaction-history")
-    public @ResponseBody ResponseEntity<Object> getTransactionHistory(@RequestParam String userUuid){
+    public @ResponseBody ResponseEntity<List<Transaction>> getTransactionHistory(@RequestParam String userUuid){
         return new ResponseEntity<>(walletService.getTransactionHistory(userUuid), HttpStatus.OK) ;
     }
 
     @GetMapping(path = "/get-user-balance")
-    public @ResponseBody ResponseEntity<Object> getUserBalance(@RequestParam String userUuid){
+    public @ResponseBody ResponseEntity<Integer> getUserBalance(@RequestParam String userUuid){
         return new ResponseEntity<>(walletService.getUserBalance(userUuid), HttpStatus.OK) ;
     }
 
     @PostMapping(path = "/create-user")
-    public @ResponseBody ResponseEntity<Object> createUser(@Valid @RequestBody CreateUserRequest createUserRequest) {
+    public @ResponseBody ResponseEntity<User> createUser(@Valid @RequestBody CreateUserRequest createUserRequest) {
         return new ResponseEntity<>(walletService.createUser(createUserRequest.getUserUuid(), createUserRequest.getName(), createUserRequest.getBalance()), HttpStatus.OK);
     }
 
